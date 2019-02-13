9d6a63483a2fca33445b1a0439b213ee4dbcf32d
olmits <42008856+olmits@users.noreply.github.com>
2019-02-13 23:22:20 +0200

    Update urls.py

--- webapp/urls.py
+++ webapp/urls.py
@@ -10,40 +10,40 @@ def before_request():
     g.db.connect()
 
 
+@app.route('/')
+@app.route('/result', methods=['POST', 'GET'])
+def get_output():
+    if request.method == 'POST':
+
+        form = request.form
+        get_payment = Payment.create_payment(form.get('currency'), form)
+        send_request = get_payment.request()
+
+        if not send_request['data'] is None:
+            PaymentsDB.update_status('Success', get_payment.payment_id)
+
+            if form['currency'] == '978':
+                return render_template("pay_page.html", result=send_request)
+
+            elif form['currency'] == '840':
+                url = send_request['data']['url']
+                return redirect(url)
+
+            else:
+                return render_template("pay_page.html", result=send_request['data'])
+        else:
+            flash(send_request['message'])
+            PaymentsDB.update_status('Error', get_payment.payment_id)
+            app.logger.debug(f'{send_request.__name__} Response error message: {send_request["message"]}')
+            return render_template('payment.html')
+    else:
+        return render_template('payment.html')
+
+
 @app.after_request
 def after_request(responce):
     g.db.close()
     return responce
-
-
-@app.route('/')
-def initial_form():
-    return render_template('payment.html')
-
-
-@app.route('/result', methods=['POST', 'GET'])
-def get_output():
-    if request.method == 'POST':
-        form = request.form
-
-        get_processor = Payment.get_subclass(form['currency'])
-        prepared_data = get_processor.form_processing(form)
-        result = prepared_data.request()
-
-        if not result['data'] is None:
-            PaymentsDB.update_status('Success', prepared_data.payment_id)
-            if form['currency'] == '978':
-                return render_template("pay_page.html", result=result)
-            elif form['currency'] == '840':
-                url = result['data']['url']
-                return redirect(url)
-            else:
-                return render_template("pay_page.html", result=result['data'])
-        else:
-            flash(result['message'])
-            PaymentsDB.update_status('Error', prepared_data.payment_id)
-            app.logger.debug(f'{get_processor.__name__} Response error message: {result["message"]}')
-            return render_template('payment.html')
 
 
 @app.errorhandler(Exception)
